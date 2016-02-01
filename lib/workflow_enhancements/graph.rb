0bd00f74e50c5dd5b6e77232c202066c0f1e1916
fredsdc <frederico.camara@serpro.gov.br>
2016-02-01 16:58:37 -0200

    * In issue, lists all roles in the project that have a workflow * Highlights workflows when you hover mouse over Role name

--- lib/workflow_enhancements/graph.rb
+++ lib/workflow_enhancements/graph.rb
@@ -1,6 +1,6 @@
 module WorkflowEnhancements::Graph
 
-	def self.load_data(roles, trackers, issue=nil)
+  def self.load_data(roles, trackers, issue=nil, project_roles=nil)
     tracker = nil
     if trackers.is_a?(Array)
       tracker = trackers.length == 1 ? trackers.first : nil
@@ -24,6 +24,7 @@ module WorkflowEnhancements::Graph
     new_issue_status_map = {}
     edges_map = {}
     WorkflowTransition.where(:tracker_id => tracker).each do |t|
+      next unless project_roles.nil? || project_roles.include?(t.role_id)
       if t.old_status_id != 0
         key = t.old_status_id.to_s + '-' + t.new_status_id.to_s
         own = role_map.include?(t.role_id)
@@ -36,15 +37,17 @@ module WorkflowEnhancements::Graph
           edges_map[key][:author] ||= author
           edges_map[key][:assignee] ||= assignee
           edges_map[key][:always] ||= always
+          edges_map[key][:roles] |= [t.role_id]
         else
           edges_map[key] = { :u => t.old_status_id, :v => t.new_status_id,
-             :own => own, :author => author, :assignee => assignee, :always => always }
+             :own => own, :author => author, :assignee => assignee, :always => always, :roles => [t.role_id] }
         end
       else
         new_issue_status_map[t.new_status_id] = 1
       end
     end
     edges_array = []
+    statuses_list = []
     edges_map.each_value do |e|
       cls = role_map.empty? ? '' : 'transOther'
       if e[:own]
@@ -54,10 +57,16 @@ module WorkflowEnhancements::Graph
           cls += ' transOwn-assignee' if e[:assignee]
         end
       end
+      if e[:roles]
+        e[:roles].each do |r|
+          cls += " role#{r}"
+        end
+      end
       edges_array << { :u => e[:u], :v => e[:v], :value => { :edgeclass => cls } }
+      statuses_list |= [e[:u], e[:v]]
     end
 
-    statuses_array = tracker.issue_statuses.map do |s|
+    statuses_array = tracker.issue_statuses.select{ |s| statuses_list.include?(s.id) }.map do |s|
       cls = ''
       if is_default_status(tracker, s)
         cls = 'state-new'
